a09cb65409475895446aa24eef473f24b4f939a9
migo <migo.vandingo@gmail.com>
2025-01-01 16:47:50 -0700

    update new table keys

--- utility/constant.py
+++ utility/constant.py
@@ -2,12 +2,18 @@ class Constant:
     @staticmethod
     def get_table_prefix(table: str) -> str:
         prefixes = {
-            "datastore": "DSTR",
-            "dataset": "DSET",
+            "datastore": "DST",
+            "datastore_roles": "DSRO",
+            "dataset": "DAT",
+            "dataset_roles": "DTRO",
             "files": "FILE",
             "project": "PROJ",
+            "project_roles": "PJRO",
             "project_git_info": "PGIT",
             "user": "USER",
+            "team": "TEAM",
+            "team_roles": "TMRO",
+
             
         }
         return prefixes[table]
@@ -16,11 +22,16 @@ class Constant:
     def get_table_id(table: str) -> str:
         tables = {
             "datastore": "datastore_id",
+            "datastore_roles": "datastore_roles_id",
             "dataset": "dataset_id",
+            "dataset_roles": "dataset_roles_id",
             "files": "file_id",
             "project": "project_id",
+            "project_roles": "project_roles_id",
             "project_git_info": "project_git_info_id",
             "user": "user_id",
+            "team": "team_id",
+            "team_roles": "team_roles_id",
             
         }
         return tables[table]
