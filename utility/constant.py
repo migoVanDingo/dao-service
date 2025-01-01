2d851f96499f01b1721217dbc2fe150eb2f16fd0
migo <migo.vandingo@gmail.com>
2025-01-01 10:36:31 -0700

    add user table keys

--- utility/constant.py
+++ utility/constant.py
@@ -6,7 +6,8 @@ class Constant:
             "dataset": "DSET",
             "files": "FILE",
             "project": "PROJ",
-            "project_git_info": "PGIT"
+            "project_git_info": "PGIT",
+            "user": "USER",
             
         }
         return prefixes[table]
@@ -18,7 +19,8 @@ class Constant:
             "dataset": "dataset_id",
             "files": "file_id",
             "project": "project_id",
-            "project_git_info": "project_git_info_id"
+            "project_git_info": "project_git_info_id",
+            "user": "user_id",
             
         }
         return tables[table]
